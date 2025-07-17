479449ebc628c333141e7566d6ff3c5ec704c3b2
Adarsh <adarshthoduvakkal@gmail.com>
2025-07-17 02:55:49 -0400

    first commit, to do list/dedication

---
+++ FinalGame_Chess.java
@@ -0,0 +1,50 @@
+//Name: Adarshkrishna Thoduvakkal
+//Date: December 15
+//Purpose: Chess Final, done on January 19
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.applet.Applet;
+
+public class FinalGame_Chess extends Applet implements ActionListener
+{
+    Panel p_card;  //to hold all of the screens
+    Panel card1, card2, card3, card4, card5, card6; //the two screens
+    CardLayout cdLayout = new CardLayout ();
+
+    //turns
+    JLabel turnpic;
+    char turn = 'w';
+    int last = -1;
+
+    //choose colors (for later, make it so pieces can swap. some things need to change in select Pawn for this to happen
+
+
+
+    //grid
+    int row = 8;
+    int col = 8;
+    JButton a[] = new JButton [row * col];
+    char piece[] [] = {{'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'}, {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},  //pieces
+	    {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	    {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'}, {'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'}};
+
+    char piecebackup[] [] = {{'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'}, {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},  //for reset
+	    {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	    {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'}, {'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'}};
+
+    char select[] [] = {{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'}, {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},  //for selected squares
+	    {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'}, {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	    {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'}, {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	    {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'}, {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'}};
+
+    char colour[] [] = {{'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}, {'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'},  //piece colour
+	    {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	    {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	    {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'}, {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'}};
+    char colourbackup[] [] = {{'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}, {'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'},  //for reset
+	    {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	    {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # StarWarsChess
-Chess made from scratch using Java Applets (very old), with a star wars theme
+Chess made from scratch using Java Applets (very old), with a star wars theme.
+
+My computer science teacher used to always tell us that 'we don't know how easy we have it with all our new IDE's and new tools'.  
+
+Using JDK 7, paper, and sheer will, I will be creating Chess from a single IO library I found from my computer science teacher's website. Then, I'll add a star wars theme and sound effects. Why not?
